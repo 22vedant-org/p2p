ac149a6dd395320116fc31c563394e0e0871eb56
vedantchinta <vedantchinta7@gmail.com>
2025-01-23 21:29:23 +0530

    better-auth init

---
+++ lib/auth.ts
@@ -0,0 +1,36 @@
+import { betterAuth, BetterAuthOptions } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { openAPI } from 'better-auth/plugins';
+import prisma from './prisma';
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
+	session: {
+		expiresIn: 60 * 60 * 24 * 7, // 7 days
+		updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60, // Cache duration in seconds
+		},
+	},
+	plugins: [openAPI()],
+	emailAndPassword: {
+		enabled: true,
+		requireEmailVerification: true,
+	},
+	emailVerification: {
+		sendOnSignUp: true,
+		autoSignInAfterVerification: true,
+		sendVerificationEmail: async ({ user, token }) => {
+			const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${process.env.EMAIL_VERIFICATION_CALLBACK_URL}`;
+			await sendEmail({
+				to: user.email,
+				subject: 'Verify your email address',
+				text: `Click the link to verify your email: ${verificationUrl}`,
+			});
+		},
+	},
+} satisfies BetterAuthOptions);
+
+export type Session = typeof auth.$Infer.Session;
