8aa2e2b5ca1712293013bc69bf46df8a8167e4e0
22vedant <vedantchinta7@gmail.com>
2025-03-20 18:15:54 +0530

    (feat)dashboard: update status cards + recent orders

--- app/(sidebar)/dashboard/page.tsx
+++ app/(sidebar)/dashboard/page.tsx
@@ -1,31 +1,13 @@
-import { authClient } from '@/lib/auth-client';
-import { ContentLayout } from '@/components/admin-panel/content-layout';
-import Dashboard from './_components/Dashboard';
-
-import { redirect } from 'next/navigation';
-// import {
-// 	getDashboardStats,
-// 	getRecentOrders,
-// 	RecentOrder,
-// } from "@/db/dashboard";
-
-export default async function DashboardPage() {
-	const session = await authClient.getSession();
-	const userId = session.data?.user.id;
-	if (!userId) {
-		return redirect('/login');
-	}
-
-	const stats = await getDashboardStats(userId);
-	const recentOrders = await getRecentOrders(userId);
-
+import React from 'react';
+import Dashboard from './_components/dashboard';
+import { TableDemo } from './_components/table';
+const page = () => {
 	return (
-		<ContentLayout title="Dashboard" className="min-h-[70vh]">
-			<Dashboard
-				name={session.data?.user.name as string}
-				stats={stats}
-				recentOrders={recentOrders as RecentOrder[]}
-			/>
-		</ContentLayout>
+		<>
+			<Dashboard />
+			{/* <TableDemo /> */}
+		</>
 	);
-}
+};
+
+export default page;
