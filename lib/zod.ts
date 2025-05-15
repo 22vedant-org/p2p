0ac19e76c72c590a7e4a31ed78dc8bc217658fab
22vedant <vedantchinta7@gmail.com>
2025-05-16 01:52:54 +0530

    fix(auth): shift modified schema to zod.ts

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,4 +1,5 @@
-import { object, string } from 'zod';
+import { phoneNumber } from 'better-auth/plugins';
+import { object, string, enum as enum_ } from 'zod';
 
 const getPasswordSchema = (type: 'password' | 'confirmPassword') =>
 	string({ required_error: `${type} is required` })
@@ -15,11 +16,28 @@ const getNameSchema = () =>
 		.min(1, 'Name is required')
 		.max(50, 'Name must be less than 50 characters');
 
+const getPhoneNumberSchema = () =>
+	string()
+		.min(10, 'Please enter a valid phone number')
+		.max(10, 'Please enter a valid phone number')
+		.optional()
+		.nullable();
+
+const getRoleSchema = () => enum_(['Rider', 'Driver']);
+
+const getGenderSchema = () => enum_(['MALE', 'FEMALE', 'OTHER']);
+
+const getCompanyNameSchema = () => string().optional();
+
 export const signUpSchema = object({
 	name: getNameSchema(),
 	email: getEmailSchema(),
 	password: getPasswordSchema('password'),
 	confirmPassword: getPasswordSchema('confirmPassword'),
+	phoneNumber: getPhoneNumberSchema(),
+	role: getRoleSchema(),
+	gender: getGenderSchema(),
+	companyName: getCompanyNameSchema(),
 }).refine((data) => data.password === data.confirmPassword, {
 	message: "Passwords don't match",
 	path: ['confirmPassword'],
